2d82da4eeb6e4a974bc647989fdcc65f19de2ff6
Veronica.C <cyj0318@gmail.com>
2016-09-07 00:16:38 +0800

    lack of enemyBullet, boom, detect bullet contact

--- src/MainScene.java
+++ src/MainScene.java
@@ -49,33 +49,31 @@ public class MainScene implements KeyListener{
     
     int bg1NewY = main.WINDOWS_HEIGHT / 2;
 	int bg2NewY = 1140;
-	int dx = 2;
+	int bgSpeed = 30;
     
     public MainScene() {//bg->fighter->enemy
-    	userKeys = new ArrayList<Integer>();//array for direction keys 
-    	
+    	userKeys = new ArrayList<Integer>();//array for direction keys     	
         _rect = new Insets(0, 0, main.WINDOWS_HEIGHT, main.WINDOWS_WIDTH);
         
+        //background Image new
         _sprite_bg1 = new Sprite(this, "res\\bg.png", main.WINDOWS_WIDTH, main.WINDOWS_HEIGHT);
         _sprite_bg2 = new Sprite(this, "res\\bg.png", main.WINDOWS_WIDTH, main.WINDOWS_HEIGHT);
-        
-        bgScrol();//set bg1 and bg2 position and scrolling    
+       
+        //set position and scrolling for bg1 and bg2 
+        bgScrol();
      
         _fighter = new Fighter(this, "res\\fighter.png", 90, 60, 3);
         SpawnFighter();
         addToScene(_fighter);
         
-        newEmemy(); //new 出敵機
-   
+        newEmemy();   
     } 
     
-    private void bgScrol(){//not finish
-//    	int bg1NewY = main.WINDOWS_HEIGHT / 2;
-//    	int bg2NewY = -768;
-//    	int dx = 10;
-    	//設定bg1初始位置
+    //scorlling bg1 and bg2
+    private void bgScrol(){   	
+    	//initial bg1 position
     	_sprite_bg1.setPosition(main.WINDOWS_WIDTH/2, bg1NewY);//(x,384)
-    	//設定bg2初始位置
+    	//initial bg2 position
     	_sprite_bg2.setPosition(main.WINDOWS_WIDTH/2, bg2NewY);//(x,-768)     
         addToScene(_sprite_bg1);
         addToScene(_sprite_bg2);
@@ -85,29 +83,24 @@ public class MainScene implements KeyListener{
         TimerTask bgTask = new TimerTask() {			
 			@Override
 			public void run() {
-				if(bg1NewY <= -375){
-					bg1NewY = 1140;
+				if(bg1NewY <= ((main.WINDOWS_HEIGHT / 2) * -1)){ //if bg1 out of top boundary 
+					bg1NewY = ((main.WINDOWS_HEIGHT /2 * 3) - 12);//reset bg1 to the button and repeat scrolling
 				}
-//				if(bg2NewY <= -375){
-//					bg2NewY = 1100;
-//				}
-				bg1NewY -= dx;
-				System.out.println("bg1NewY: "+bg1NewY);
+				if(bg2NewY <= ((main.WINDOWS_HEIGHT / 2) *-1)){
+					bg2NewY = ((main.WINDOWS_HEIGHT /2 * 3) - 12);
+				}
+				bg1NewY -= 1;//arg. for moving bg1.y
+//				System.out.println("bg1NewY: "+bg1NewY);
 				_sprite_bg1.setPosition(main.WINDOWS_WIDTH/2, bg1NewY);//(x,384)
 				
-				bg2NewY -= dx;
-				System.out.println("bg2NewY: "+bg2NewY);
+				bg2NewY -= 1;
+//				System.out.println("bg2NewY: "+bg2NewY);
 				_sprite_bg2.setPosition(main.WINDOWS_WIDTH/2, bg2NewY);//(x,-768)  
 			}
-		};
-		
-		bgTimer.schedule(bgTask, 0, 10);
-        
-        
+		};		
+		bgTimer.schedule(bgTask, 0, bgSpeed);
     }
     
-    
-
     //重置飛機位置
     private void SpawnFighter(){
         _fighter.setPosition(main.WINDOWS_WIDTH / 2, main.WINDOWS_HEIGHT - 60);
@@ -134,8 +127,6 @@ public class MainScene implements KeyListener{
     public void update(){        
         updateFrame();
     }
-    
-    
 
     //移除場景物件
     private void removeFromScene(Sprite sprite){
@@ -153,7 +144,7 @@ public class MainScene implements KeyListener{
         _render_objects.add(new RenderLayer(sprite, layer));
     }
 
- //key event
+ //key listener
 	@Override
 	public void keyPressed(KeyEvent e) {
 		//if figher.x>0 then move
@@ -198,16 +189,11 @@ public class MainScene implements KeyListener{
   //move fighter
 	protected void _fighter_move(){
 		int x = _fighter._x;
-		int y = _fighter._y;		
-//		int bgy = _sprite_bg._y;
-		
+		int y = _fighter._y;				
 		if(userKeys.contains(KeyEvent.VK_UP)){
 			if((y-35)>0){
 				y -= Velocity_Fighter;	
-			}
-//			bgy -= bgDX;
-//			_sprite_bg.setPosition(main.WINDOWS_WIDTH/2, bgy);
-	
+			}	
 		}
 		
 		if((y+35)<main.WINDOWS_HEIGHT){
